2bf3e215931152eb1ef4e2f16a0de97997ffb472
David delhart <dedelhart@voicebase.com>
2020-01-26 12:17:57 -0800

    update property formats

--- site/js/containers/pages/Beta/beta.store.js
+++ site/js/containers/pages/Beta/beta.store.js
@@ -3,10 +3,7 @@ import { ValueStream } from '@wonderlandlabs/looking-glass-engine';
 export default (props) => {
   const BetaStore = new ValueStream('betaStore')
     .method('inc', (s) => s.setCount(s.my.count + 1))
-    .property('count', {
-      start: 1,
-      type: 'integer',
-    });
+    .property('count', 1, 'integer');
 
   return BetaStore;
 };
--- site/js/store/site.store.js
+++ site/js/store/site.store.js
@@ -2,9 +2,6 @@ import { ValueStream } from '@wonderlandlabs/looking-glass-engine';
 
 const SiteStore = new ValueStream('siteStore')
   .method('inc', (s) => s.setCount(s.my.count + 1))
-  .property('count', {
-    start: 1,
-    type: 'integer',
-  });
+  .property('count', 1, 'integer');
 
 export default SiteStore;
